3de4bf5712845fb64db8e50cbb9674a093e32d42
Brandon Yee <brandon.yee150@gmail.com>
2019-08-16 12:58:24 -1000

    Testing Google Signin

--- src/js/client.js
+++ src/js/client.js
@@ -12,3 +12,11 @@ var socket = io.connect();
 socket.on("connectionReceived", function connectionReceived () {
 	// 
 });
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
