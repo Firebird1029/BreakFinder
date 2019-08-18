a6b668965e30196b044ee2432807924109e20809
Jason Tay <jtay20@punahou.edu>
2019-08-17 17:23:52 -1000

    Fixed so that student id is not needed

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var debug = false;
  * http://html2jade.vida.io/
  * https://fontawesome.com/icons
  */
+
 // Load Node Dependencies & Custom Modules
 var express = require("express"),
 	app = express(),
@@ -41,14 +42,14 @@ app.use((req, res, next) => {
 	res.status(404).render("404.pug");
 });
 
-// Using cheerio to process the schedule that was extracted by nightmare
-function getData(html) {
+// Use cheerio to process schedule data and find breaks
+function getDataFromTable(html) {
 	var data = [[],[],[],[],[],[]];
 	const $ = cheerio.load(html);
 	for (var i = 1; i < 33; i++) {
 		for (var j = 2; j < 8; j++) {
 			var currentCell = $("td.screenCell tbody:nth-child(2) tr:nth-child(" + i + ")" + " td:nth-child(" + j + ")");
-			/* If there is more than 1 <br> then there is a class in that particular cell
+			/* If there is more than 1 <br> in the <td> then there is a class in that particular cell
 			 * Push a 1 into the array if you have a class, a 0 if you don't
 			 */
 			if (currentCell.children().length > 1) {
@@ -59,12 +60,12 @@ function getData(html) {
 		}
 	}
 	console.log(data);
-} 
+}
 
 /* Pull student schedule from myBackpack using the username and password to login
  * The studentNum is the student's id number, which is used to select the correct schedule
  */
-function getStudentData(username, password, studentNum) {
+function getStudentData(username, password) {
 	nightmare
 		.goto('https://mybackpack.punahou.edu/SeniorApps/facelets/registration/loginCenter.xhtml')
 		.wait('body')
@@ -73,22 +74,33 @@ function getStudentData(username, password, studentNum) {
 		.click('input[name="form:signIn"]')
 		.wait("a[href='https://mybackpack.punahou.edu/SeniorApps/studentParent/schedule.faces?selectedMenuId=true']")
 		.click("a[href='https://mybackpack.punahou.edu/SeniorApps/studentParent/schedule.faces?selectedMenuId=true']")
-		.wait("select") 
-		.select("select", studentNum)
-		.wait(1000)
-		.click("input.chartButtonUp")
-		.wait(5000)
-		.evaluate(() => document.body.innerHTML)
-		.then(response => {
-			getData(response);
+		.wait("select")
+		.evaluate(function(){
+			return document.querySelector("select").children[1].value;
+		})
+		.then(value => {
+			nightmare
+				.select('select', value)
+				.wait(1000)
+				.click("input.chartButtonUp")
+				.wait(1000)
+				.evaluate(() => document.body.innerHTML)
+				.then(response => {
+					getDataFromTable(response);
+				})
+				.catch(error => {
+					console.error('Search failed:', error);
+				})
+			nightmare.end();
 		})
 		.catch(error => {
 			console.error('Search failed:', error);
+			// Failed login or something wrong
 		})
-	nightmare.end();
 }
 
-getStudentData("USERNAME HERE TO TEST", "PASSWORD HERE TO TEST", "STUDENT NUMBER HERE");
+// For testing
+getStudentData("USERNAME HERE", "PASSWORD HERE");
 
 // var router = require("./routes/routes.js");
 // app.use("/", router);
