bb8877276fe5388b174647ca0a21587438abd0ea
Jason Tay <jtay20@punahou.edu>
2019-08-17 14:39:07 -1000

    Working headless browser for getting schedule data

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ var debug = false;
  * http://html2jade.vida.io/
  * https://fontawesome.com/icons
  */
-
 // Load Node Dependencies & Custom Modules
 var express = require("express"),
 	app = express(),
@@ -25,6 +24,9 @@ var express = require("express"),
 	// Project-Specific Dependencies
 	io = require("socket.io"),
 	listener = io.listen(server),
+	Nightmare = require("nightmare"),
+	nightmare = Nightmare({ show: false }),
+	cheerio = require("cheerio"),
 
 	// Utilities & Custom Modules
 	utils = require("./utils.js");
@@ -38,6 +40,55 @@ app.use(pugStatic(__dirname + "/views"));
 app.use((req, res, next) => {
 	res.status(404).render("404.pug");
 });
+
+// Using cheerio to process the schedule that was extracted by nightmare
+function getData(html) {
+	var data = [[],[],[],[],[],[]];
+	const $ = cheerio.load(html);
+	for (var i = 1; i < 33; i++) {
+		for (var j = 2; j < 8; j++) {
+			var currentCell = $("td.screenCell tbody:nth-child(2) tr:nth-child(" + i + ")" + " td:nth-child(" + j + ")");
+			/* If there is more than 1 <br> then there is a class in that particular cell
+			 * Push a 1 into the array if you have a class, a 0 if you don't
+			 */
+			if (currentCell.children().length > 1) {
+				data[j - 2].push(1);
+			} else {
+				data[j - 2].push(0);
+			}
+		}
+	}
+	console.log(data);
+} 
+
+/* Pull student schedule from myBackpack using the username and password to login
+ * The studentNum is the student's id number, which is used to select the correct schedule
+ */
+function getStudentData(username, password, studentNum) {
+	nightmare
+		.goto('https://mybackpack.punahou.edu/SeniorApps/facelets/registration/loginCenter.xhtml')
+		.wait('body')
+		.type('input[id = "form:userId"]', username)
+		.type('input[id = "form:userPassword"]', password)
+		.click('input[name="form:signIn"]')
+		.wait("a[href='https://mybackpack.punahou.edu/SeniorApps/studentParent/schedule.faces?selectedMenuId=true']")
+		.click("a[href='https://mybackpack.punahou.edu/SeniorApps/studentParent/schedule.faces?selectedMenuId=true']")
+		.wait("select") 
+		.select("select", studentNum)
+		.wait(1000)
+		.click("input.chartButtonUp")
+		.wait(5000)
+		.evaluate(() => document.body.innerHTML)
+		.then(response => {
+			getData(response);
+		})
+		.catch(error => {
+			console.error('Search failed:', error);
+		})
+	nightmare.end();
+}
+
+getStudentData("USERNAME HERE TO TEST", "PASSWORD HERE TO TEST", "STUDENT NUMBER HERE");
 
 // var router = require("./routes/routes.js");
 // app.use("/", router);
