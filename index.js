a9cab087dc263ef741004055ba7613e9369a59f8
Brandon Yee <brandon.yee150@gmail.com>
2019-08-20 20:06:44 -1000

    Mongodb

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ var express = require("express"),
 	// Project-Specific Dependencies
 	io = require("socket.io"),
 	listener = io.listen(server),
+	mongo = require("mongodb"),
+	MongoClient = mongo.MongoClient,
 
 	// Utilities & Custom Modules
 	utils = require("./utils.js");
@@ -52,18 +54,31 @@ listener.sockets.on("connection", function connectionDetected (socket) {
 		const client = new OAuth2Client("831400703769-dgio9hsp0mh3hmndc4rg1ljaakusbolu.apps.googleusercontent.com"); // "831400703769-dgio9hsp0mh3hmndc4rg1ljaakusbolu"
 		var userid;
 		async function verify() {
-		  const ticket = await client.verifyIdToken({
-		      idToken: options.idToken,
-		      audience: "831400703769-dgio9hsp0mh3hmndc4rg1ljaakusbolu.apps.googleusercontent.com",  // Specify the CLIENT_ID of the app that accesses the backend
-		      // Or, if multiple clients access the backend:
-		      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-		  });
-		  const payload = ticket.getPayload();
-		  userid = payload['sub'];
-		  console.log(payload);
-		  // If request specified a G Suite domain:
-		  //const domain = payload['hd'];
+			const ticket = await client.verifyIdToken({
+					idToken: options.idToken,
+					audience: "831400703769-dgio9hsp0mh3hmndc4rg1ljaakusbolu.apps.googleusercontent.com",	// Specify the CLIENT_ID of the app that accesses the backend
+					// Or, if multiple clients access the backend:
+					//[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+			});
+			const payload = ticket.getPayload();
+			userid = payload['sub'];
+			console.log(payload);
+			// If request specified a G Suite domain:
+			//const domain = payload['hd'];
 		}
 		verify().catch(console.error);
+	});
+});
+
+// MongoDB
+var mongoUrl = "mongodb://localhost:27017/";
+MongoClient.connect(mongoUrl, function(err, db) {
+	if (err) throw err;
+	console.log("Database created!");
+	var dbo = db.db("mydb");
+	dbo.createCollection("customers", function (err, res) {
+		if (err) throw err;
+		console.log("Collection created!");
+		db.close();
 	});
 });
